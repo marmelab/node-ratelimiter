e92aff6861c0635c833f2f56b7f7e5fbb7a90cf7
djhi <gildas.garcia@gmail.com>
2016-03-16 14:00:09 +0100

    Use 2 spaces instead of 4 added lib to gitignore

--- src/adapters/null.spec.js
+++ src/adapters/null.spec.js
@@ -2,16 +2,16 @@ import 'should';
 import adapterFactory from './null';
 
 describe('NullAdapter', () => {
-    describe('.newHit', () => {
-        it('should return a fixed result', done => {
-            const adapter = adapterFactory()('foo', 5, 100000);
-            adapter.newHit()
-            .then(res => {
-                res.remaining.should.equal(1000);
-                res.total.should.equal(1000);
-            })
-            .then(done)
-            .catch(done);
-        });
+  describe('.newHit', () => {
+    it('should return a fixed result', done => {
+      const adapter = adapterFactory()('foo', 5, 100000);
+      adapter.newHit()
+      .then(res => {
+        res.remaining.should.equal(1000);
+        res.total.should.equal(1000);
+      })
+      .then(done)
+      .catch(done);
     });
+  });
 });
